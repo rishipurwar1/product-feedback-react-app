55b7bbfcd86ca17d6becdc6a69ff78c71023aa4f
Rishi Purwar <57035408+rishipurwar1@users.noreply.github.com>
2021-10-17 18:23:13 +0530

    add vercel.json file

--- server/app.js
+++ server/app.js
@@ -15,5 +15,14 @@ connectDB();
 app.use("/feedbacks", feedbacksRoutes);
 app.use("/user", userRoutes);
 
+if (process.env.NODE_ENV == "production") {
+  const path = require("path");
+
+  app.get("/", (req, res) => {
+    app.use(express.static(path.resolve(__dirname, "client", "build")));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
